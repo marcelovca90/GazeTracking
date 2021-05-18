fdfaeb585f8bb24a91615333895188604ab92b1c
Marcelo Cysneiros <marcelovca90@gmail.com>
2021-05-18 11:46:32 -0300

    Persist coordinates to CSV file.

--- example.py
+++ example.py
@@ -4,7 +4,10 @@ Check the README.md for complete documentation.
 """
 
 import cv2
+import pandas as pd
 from gaze_tracking import GazeTracking
+
+coords = []
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
@@ -34,8 +37,24 @@ while True:
     right_pupil = gaze.pupil_right_coords()
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    coords.append([left_pupil,right_pupil])
 
     cv2.imshow("Demo", frame)
 
     if cv2.waitKey(1) == 27:
         break
+
+# find empty rows
+empty_rows = []
+for i in range(len(coords)):
+    if coords[i] == [None,None]:
+        empty_rows.append(i)
+
+# prepare data to be saved
+df = pd.DataFrame(coords, columns=['left_pupil','right_pupil'])
+
+# remove empty rows
+df.drop(empty_rows, inplace=True)
+
+# save data to filesystem
+df.to_csv('coords.csv', index=False)
